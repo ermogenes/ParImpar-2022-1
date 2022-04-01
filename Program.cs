b4b30c05aeaf8d5201e0c8796cf58794b954d742
Ermogenes Palacio <ermogenes.palacio@etec.sp.gov.br>
2022-04-01 20:21:38 -0300

    Finaliza exemplo

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int numeroDigitado;
+
+Console.Write("Digite um número: ");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+bool numeroEhPar = (numeroDigitado % 2) == 0;
+
+if (numeroEhPar)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine($"{numeroDigitado} é par");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine($"{numeroDigitado} é ímpar");
+}
+
+Console.ResetColor();
